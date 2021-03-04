dfaa86747285e8893f4cf44b4469a9f9d0c10cc5
Mostafa-Code19 <tabrizian.codes@gmail.com>
2021-03-04 21:50:00 +0330

    Create all scss files and variables with responsive method + header completed just the menu left

---
+++ app/js/script.js
@@ -0,0 +1,25 @@
+const log = (code) => {
+    console.log(code)
+}
+
+
+
+const categoriesBtn = document.querySelector('.header__categories__button')
+categoriesBtn.addEventListener('click', appearCategories)
+function appearCategories() {
+
+    const categories = document.querySelector('.header__categories')
+    const arw = document.querySelector('.arw')
+    const categoriesStyle = getComputedStyle(categories)
+    const close = categoriesStyle.opacity == 0
+
+    if (close) {
+        categories.classList.add('header__categories__open')
+        arw.classList.add('arw__open')
+
+    } else {
+        categories.classList.remove('header__categories__open')
+        arw.classList.remove('arw__open')
+
+    }
+}
