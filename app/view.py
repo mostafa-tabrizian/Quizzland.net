8532b73782a88ed898f1d85e86d7fa63726dd384
Mostafa-Code19 <tabrizian.codes@gmail.com>
2021-05-13 19:42:37 +0430

    change name of pageNotFound to 404 and added: 403:forbbiden and 500:serverError page

--- app/view.py
+++ app/view.py
@@ -7,9 +7,40 @@ app = Flask(__name__)
 def Main():
     return render_template('index.html', sortNewest = sortNewest(), sortMostViews = sortMostViews(), header__noBg = 'header__noBg')
 
-@app.route('/<str>')
-def string(str):
-    return render_template('notFound.html')
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    userSearchInput = request.form['userSearchInput']
+    if request.method == 'POST':
+        userSearchInputInCategoriesDb_far = s.query(Categories).filter(Categories.title_far.ilike(f'%{userSearchInput}%')).all()
+        userSearchInputInCategoriesDb_eng = s.query(Categories).filter(Categories.title_eng.ilike(f'%{userSearchInput}%')).all()
+        userSearchInputInQuizzesDb_far = s.query(Quizzes).filter(Quizzes.title_far.ilike(f'%{userSearchInput}%')).all()
+        userSearchInputInQuizzesDb_eng = s.query(Quizzes).filter(Quizzes.title_eng.ilike(f'%{userSearchInput}%')).all()
+
+        return render_template('searchResult.html', 
+            userSearchInput = userSearchInput,
+            userSearchInputInCategoriesDb_far = userSearchInputInCategoriesDb_far,
+            userSearchInputInCategoriesDb_eng = userSearchInputInCategoriesDb_eng,
+            userSearchInputInQuizzesDb_far = userSearchInputInQuizzesDb_far,
+            userSearchInputInQuizzesDb_eng = userSearchInputInQuizzesDb_eng
+            )
+    else:
+        return render_template('404.html')
+
+@app.route('/search/<searchMoreOfThis>')
+def moreResult(searchMoreOfThis):
+    userSearchInput = searchMoreOfThis
+    userSearchInputInCategoriesDb_far = s.query(Categories).filter(Categories.title_far.ilike(f'%{userSearchInput}%')).all()
+    userSearchInputInCategoriesDb_eng = s.query(Categories).filter(Categories.title_eng.ilike(f'%{userSearchInput}%')).all()
+    userSearchInputInQuizzesDb_far = s.query(Quizzes).filter(Quizzes.title_far.ilike(f'%{userSearchInput}%')).all()
+    userSearchInputInQuizzesDb_eng = s.query(Quizzes).filter(Quizzes.title_eng.ilike(f'%{userSearchInput}%')).all()
+
+    return render_template('moreSearchResult.html', 
+        userSearchInput = userSearchInput,
+        userSearchInputInCategoriesDb_far = userSearchInputInCategoriesDb_far,
+        userSearchInputInCategoriesDb_eng = userSearchInputInCategoriesDb_eng,
+        userSearchInputInQuizzesDb_far = userSearchInputInQuizzesDb_far,
+        userSearchInputInQuizzesDb_eng = userSearchInputInQuizzesDb_eng
+    )
 
 @app.route('/category/<category>/<int:page>')
 def Category(category, page):
@@ -38,6 +69,15 @@ def result(title):
         fullTitle = fullTitle + ' ' + title
     return render_template('/result.html', fullTitle = fullTitle)
 
+
+@app.route('/about')
+def about():
+    return render_template('/about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('/contact.html')
+
 @app.route('/newsletter', methods=['GET', 'POST'])
 def newsletter():
     userEmail = request.form['userEmail']
@@ -55,45 +95,14 @@ def newsletter():
         else:
             return render_template('/no-success.html')
 
-@app.route('/about')
-def about():
-    return render_template('/about.html')
+@app.errorhandler(404)
+def pageNotFound(e):
+    return render_template('404.html'), 404
 
-@app.route('/contact')
-def contact():
-    return render_template('/contact.html')
-
-@app.route('/search', methods=['GET', 'POST'])
-def search():
-    userSearchInput = request.form['userSearchInput']
-    if request.method == 'POST':
-        userSearchInputInCategoriesDb_far = s.query(Categories).filter(Categories.title_far.ilike(f'%{userSearchInput}%')).all()
-        userSearchInputInCategoriesDb_eng = s.query(Categories).filter(Categories.title_eng.ilike(f'%{userSearchInput}%')).all()
-        userSearchInputInQuizzesDb_far = s.query(Quizzes).filter(Quizzes.title_far.ilike(f'%{userSearchInput}%')).all()
-        userSearchInputInQuizzesDb_eng = s.query(Quizzes).filter(Quizzes.title_eng.ilike(f'%{userSearchInput}%')).all()
-
-        return render_template('searchResult.html', 
-            userSearchInput = userSearchInput,
-            userSearchInputInCategoriesDb_far = userSearchInputInCategoriesDb_far,
-            userSearchInputInCategoriesDb_eng = userSearchInputInCategoriesDb_eng,
-            userSearchInputInQuizzesDb_far = userSearchInputInQuizzesDb_far,
-            userSearchInputInQuizzesDb_eng = userSearchInputInQuizzesDb_eng
-            )
-    else:
-        return render_template('notFound.html')
-
-@app.route('/search/<searchMoreOfThis>')
-def moreResult(searchMoreOfThis):
-    userSearchInput = searchMoreOfThis
-    userSearchInputInCategoriesDb_far = s.query(Categories).filter(Categories.title_far.ilike(f'%{userSearchInput}%')).all()
-    userSearchInputInCategoriesDb_eng = s.query(Categories).filter(Categories.title_eng.ilike(f'%{userSearchInput}%')).all()
-    userSearchInputInQuizzesDb_far = s.query(Quizzes).filter(Quizzes.title_far.ilike(f'%{userSearchInput}%')).all()
-    userSearchInputInQuizzesDb_eng = s.query(Quizzes).filter(Quizzes.title_eng.ilike(f'%{userSearchInput}%')).all()
-
-    return render_template('moreSearchResult.html', 
-        userSearchInput = userSearchInput,
-        userSearchInputInCategoriesDb_far = userSearchInputInCategoriesDb_far,
-        userSearchInputInCategoriesDb_eng = userSearchInputInCategoriesDb_eng,
-        userSearchInputInQuizzesDb_far = userSearchInputInQuizzesDb_far,
-        userSearchInputInQuizzesDb_eng = userSearchInputInQuizzesDb_eng
-        )
+@app.errorhandler(403)
+def forbidden(e):
+    return render_template('403.html'), 403
+    
+@app.errorhandler(500)
+def internalServerError(e):
+    return render_template('500.html'), 500
