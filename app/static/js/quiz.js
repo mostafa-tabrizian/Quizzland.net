8e4accdb041246bcd6a1dfd81d4ce91f653f875d
Mostafa-Code19 <tabrizian.codes@gmail.com>
2021-05-31 10:28:05 +0430

    created function which in quiz after the user chose the answer show him/her the result of that question | added if its pointyQuiz show the last question arw | change the quiz function from script.js to quiz.js

--- app/static/js/quiz.js
+++ app/static/js/quiz.js
@@ -1,18 +1,18 @@
-// Grab the answers
+encodedNumbers = {
+    1: 'b25l',
+    2: 'dHdv',
+    3: 'dGhyZWU=',
+    4: 'Zm91cg==',
+    'b25l': 1,
+    'dHdv': 2,
+    'dGhyZWU=': 3,
+    'Zm91cg==': 4,
+}
 
-// add value to correct question
 
-
-const typeOfQuiz = localStorage.getItem('OR4XAZKPMZIXK2L2')
-
+const typeOfQuiz = quiz__questions.getAttribute('tag')
 if (quiz__container__eachOne) {
     if (typeOfQuiz == 'quiz') {
-        encodedNumbers = {
-            1: 'b25l',
-            2: 'dHdv',
-            3: 'dGhyZWU=',
-            4: 'Zm91cg=='
-        }
         quiz__container__eachOne.forEach(each => {
             const answerData = each.getAttribute('data')
             each[answerData - 1].value = 'Y29ycmVjdA=='
@@ -49,6 +49,185 @@ if (quiz__container__eachOne) {
         } else { log('no result page') } 
     }
 } else { log('no quiz.js') }
+
+//----------------------------------------------------------
+
+// quiz questions
+if (quiz__questions) {
+
+    const numberOfQuestions = quiz__container.length
+    let currentQuestion = 1
+
+    quiz__numberOfQuestions.innerHTML = `سوال&nbsp:&nbsp&nbsp${numberOfQuestions}`
+    quiz__questionCounter__totalQuestions.innerHTML = numberOfQuestions
+    quiz__questionCounter__totalAnswered.innerHTML = currentQuestion
+    
+    const pauseTheFunctionOfChangingQuestions = () => {
+        log(typeOfQuiz)
+        quiz__questionChanger__next.classList.add('pointerOff')
+        if (typeOfQuiz == 'pointy') {
+            quiz__questionChanger__last.classList.add('pointerOff')
+        }
+        setTimeout(() => {
+            quiz__questionChanger__next.classList.remove('pointerOff')
+            if (typeOfQuiz == 'pointy') {
+                quiz__questionChanger__last.classList.remove('pointerOff')
+            }
+        }, 1500)
+    }
+
+    let FinalTitleOfQuiz = ''
+    const titleOfTheQuiz = () => {
+        const titleOfQuiz = document.querySelector('.quiz__head h3').innerText
+        const splittedTitleOfQuiz = titleOfQuiz.split(' ')
+        splittedTitleOfQuiz.forEach(each => {
+            FinalTitleOfQuiz += `-${each}`
+        })
+        return FinalTitleOfQuiz
+    }
+    
+    const checkIfTheQuizEndedAndShowResultPage = () => {
+        let FinalTitleOfQuiz = ''
+        if (currentQuestion - 1 == numberOfQuestions) {
+            FinalTitleOfQuiz = titleOfTheQuiz()
+            if (typeOfQuiz == 'quiz') {
+                calculateResult(FinalTitleOfQuiz)
+            } else if (typeOfQuiz == 'pointy') {
+                calculateResult_pointy(FinalTitleOfQuiz)
+            }
+        }
+    }
+
+    const plusOneToAnsweredQuestionsIfItsNotTheLast = () => {
+        if (currentQuestion != numberOfQuestions + 1) {
+            currentQuestion += 1
+            quiz__questionCounter__totalAnswered.innerHTML = currentQuestion
+        }
+    }
+
+    const next = -1076.05
+    const last = +1076.05
+    const goToAnotherQuestion = (changeToWhat) => {
+            plusOneToAnsweredQuestionsIfItsNotTheLast()
+            quiz__container.forEach(each => {
+                currQuestionPosition = parseInt(getComputedStyle(each).transform.split(', ')[4])
+                currQuestionPosition = currQuestionPosition + changeToWhat
+                each.style.transform = `translate(${currQuestionPosition}px)`
+            })
+    }
+
+    const goToAnotherQuestionWithDelay = (changeToWhat) => {
+        setTimeout(() => {
+            goToAnotherQuestion(changeToWhat)
+        }, 3000);
+    }
+
+    const shouldLetTheUserChooseAnotherOption = (toDo) => {
+        // toDo = let or doNot
+        if (toDo == 'doNot') {
+            quiz__questions.classList.add('pointerOff')
+        } else {
+            quiz__questions.classList.remove('pointerOff')
+        }
+    }
+
+    const whatIsTheAnswer = () => {
+        let answerOfQuestion = quiz__container__eachOne[currentQuestion - 1].getAttribute('data')
+        answerOfQuestion = encodedNumbers[answerOfQuestion]
+        return answerOfQuestion
+    }
+
+    const whatIsTheUserAnswer = (optionUserChose) => {
+        optionUserChose = optionUserChose.id.slice(-1)
+        return optionUserChose
+    }
+
+    const showTheCorrectAnswer = (correctAnswer) => {
+        const currentQuestionOptions = quiz__container__eachOne[currentQuestion - 1]
+        const correctOption = currentQuestionOptions.querySelectorAll(`label`)[correctAnswer - 1]
+        correctOption.classList.add('quiz__correctAnswer')
+    }
+
+    const checkUserAnswer = (each) => {
+        const correctAnswer = whatIsTheAnswer()
+        const userChose = whatIsTheUserAnswer(each)
+        const userOption = `label[id='${each.id}']`
+        const userOptionDOM = document.querySelector(userOption)
+        if (userChose == correctAnswer) {
+            userOptionDOM.classList.add('quiz__correctAnswer')
+        } else {
+            userOptionDOM.classList.add('quiz__wrongAnswer')
+            showTheCorrectAnswer(correctAnswer)
+        }
+    }
+
+    quiz__options.forEach(each => {
+        each.addEventListener('click', () => {
+            const switchBtn = quiz__autoQuestionChangerSwitch__innerBtn
+            checkIfTheQuizEndedAndShowResultPage()
+            pauseTheFunctionOfChangingQuestions()
+            if (typeOfQuiz == 'quiz') {
+                shouldLetTheUserChooseAnotherOption('doNot')
+                checkUserAnswer(each)
+            }
+            if (!(switchBtn.classList.contains('quiz__autoQuestionChangerSwitch__innerBtn__switched'))) {
+                goToAnotherQuestionWithDelay(next)
+                if(typeOfQuiz == 'quiz') {
+                    shouldLetTheUserChooseAnotherOption('let')
+                }
+            }
+        })
+    })
+
+    quiz__autoQuestionChangerSwitch.addEventListener('click', () => {
+        const switchBtn = quiz__autoQuestionChangerSwitch__innerBtn
+        if (switchBtn.classList.contains('quiz__autoQuestionChangerSwitch__innerBtn__switched')) {
+            quiz__autoQuestionChangerSwitch__innerBtn.classList.remove('quiz__autoQuestionChangerSwitch__innerBtn__switched')
+            fadeOut(quiz__questionChanger__next)
+            if (typeOfQuiz == 'pointy') {
+                fadeOut(quiz__questionChanger__last)
+            }
+        } else {
+            quiz__autoQuestionChangerSwitch__innerBtn.classList.add('quiz__autoQuestionChangerSwitch__innerBtn__switched')
+            fadeIn(quiz__questionChanger__next)
+            if (typeOfQuiz == 'pointy') {
+                fadeIn(quiz__questionChanger__last)
+            }
+        }
+    })
+
+    quiz__questionChanger__next.addEventListener('click', () => {
+        goToAnotherQuestion(next)
+        pauseTheFunctionOfChangingQuestions()
+        checkIfTheQuizEndedAndShowResultPage()
+        shouldLetTheUserChooseAnotherOption('let')
+    })
+
+    const minusOneToAnsweredQuestionsIfItsNotTheFirst = () => {
+        if (currentQuestion != 1) {
+            currentQuestion -= 1
+            quiz__questionCounter__totalAnswered.innerHTML = currentQuestion
+
+        }
+    }
+
+    quiz__questionChanger__last.addEventListener('click', () => {
+        let currQuestionPosition = parseInt(getComputedStyle(quiz__container[0]).transform.split(', ')[4])
+        if (currQuestionPosition > -500) {
+            return "there is no last question"
+        }
+        minusOneToAnsweredQuestionsIfItsNotTheFirst()
+        goToAnotherQuestion(last)
+        pauseTheFunctionOfChangingQuestions()
+    }) 
+
+} else {log('no nextQuestion btn')}
+
+
+
+// -------------------------------------------------------------------------
+
+
 
 const calculateResult = (FinalTitleOfQuiz) => {
     let quizUrl = decodeURIComponent(window.location.href)
