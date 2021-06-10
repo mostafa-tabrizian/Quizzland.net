a909ea585f8d4c7cbfebda99dd9098b67d2efd16
Mostafa-Code19 <tabrizian.codes@gmail.com>
2021-06-10 12:21:22 +0430

    changed the admin url from 'admin' to 'theKingAlexanderJoseph

--- Quizland/urls.py
+++ Quizland/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('app.urls')),
-    path('admin/', admin.site.urls),
+    path('theKingAlexanderJoseph/', admin.site.urls),
 ]
 
 handler404 = 'app.views.error404'
