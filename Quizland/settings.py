e2d7e44048c365ac62127f2062de80741b4d589d
Mostafa-Code19 <tabrizian.codes@gmail.com>
2021-06-10 21:51:45 +0430

    reinstall the venv

--- Quizland/settings.py
+++ Quizland/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-57hi20*x0$r=7ea$gfaky6htoai24bou9mjxi7spob7z1$+89g
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'app.apps.AppConfig',
