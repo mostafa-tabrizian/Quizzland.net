d706f4dc136a58d1a756e5331c94fe5696fb576a
Mostafa-Code19 <tabrizian.codes@gmail.com>
2021-06-20 06:22:37 +0430

    migrate undo in vs code to do in cpanel host

--- app/admin.py
+++ app/admin.py
@@ -2,14 +2,7 @@ from django.contrib import admin
 import django.contrib.auth.admin
 import django.contrib.auth.models
 from django.contrib import auth
-from .models import Document, Document_Admin,\
-                    SubCategories, SubCategories_Admin,\
-                    Quizzes, Quizzes_Admin,\
-                    Quizzes_Pointy, Quizzes_Pointy_Admin,\
-                    Questions, Questions_Admin,\
-                    Pointy_Questions, Pointy_Questions_Admin,\
-                    Newsletter_Users, Newsletter_Users_Admin,\
-                    Thumbnil_SubCategory, Thumbnil_SubCategory_Admin
+from .models import *
 
 admin.site.site_header = "Quizland Admin Panel"
 
@@ -20,6 +13,6 @@ admin.site.register(Quizzes_Pointy, Quizzes_Pointy_Admin)
 admin.site.register(Questions, Questions_Admin)
 admin.site.register(Pointy_Questions, Pointy_Questions_Admin)
 admin.site.register(Newsletter_Users, Newsletter_Users_Admin)
-admin.site.register(Thumbnil_SubCategory, Thumbnil_SubCategory_Admin)
+admin.site.register(Thumbnil_SubCategory)
 
 admin.site.unregister(auth.models.Group)
