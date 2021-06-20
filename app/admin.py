1b91d2c8d69c0c99accba03df3d8e29a1177e58a
Mostafa-Code19 <tabrizian.codes@gmail.com>
2021-06-20 06:00:37 +0430

    Thumnail_SubCategory models added to admin panel

--- app/admin.py
+++ app/admin.py
@@ -8,7 +8,8 @@ from .models import Document, Document_Admin,\
                     Quizzes_Pointy, Quizzes_Pointy_Admin,\
                     Questions, Questions_Admin,\
                     Pointy_Questions, Pointy_Questions_Admin,\
-                    Newsletter_Users, Newsletter_Users_Admin
+                    Newsletter_Users, Newsletter_Users_Admin,\
+                    Thumbnil_SubCategory, Thumbnil_SubCategory_Admin
 
 admin.site.site_header = "Quizland Admin Panel"
 
@@ -19,5 +20,6 @@ admin.site.register(Quizzes_Pointy, Quizzes_Pointy_Admin)
 admin.site.register(Questions, Questions_Admin)
 admin.site.register(Pointy_Questions, Pointy_Questions_Admin)
 admin.site.register(Newsletter_Users, Newsletter_Users_Admin)
+admin.site.register(Thumbnil_SubCategory, Thumbnil_SubCategory_Admin)
 
 admin.site.unregister(auth.models.Group)
