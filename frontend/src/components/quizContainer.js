e61b8afda668ae68d411e4c3fe1f9e68a9ba9d19
Mostafa-Code19 <tabrizian.codes@gmail.com>
2021-08-06 23:28:24 +0430

    the date publish of quiz will show as days, months, years spent after the publish. not as a full date

--- frontend/src/components/quizContainer.js
+++ frontend/src/components/quizContainer.js
@@ -18,6 +18,30 @@ const QuizContainer = (props) => {
         }
     }, [])
 
+    const datePublishHandler = (publishFullDate) => {
+        if (publishFullDate) {
+            const publishDay = parseInt(publishFullDate.slice(8, 10))
+            const publishMonth = parseInt(publishFullDate.slice(5, 7))
+            const publishYear = parseInt(publishFullDate.slice(1, 4)) + 2000
+
+            const currentDay = new Date().getDay() + 1
+            const currentMonth = new Date().getMonth() + 1
+            const currentYear = new Date().getFullYear()
+
+            if (currentYear > publishYear) {
+                const totalYearsAfterPublishingTheQuiz = currentYear - publishYear
+                return `${totalYearsAfterPublishingTheQuiz} سال پیش`
+            } else if (currentMonth > publishMonth){
+                const totalMonthsAfterPublishingTheQuiz = currentMonth - publishMonth
+                return `${totalMonthsAfterPublishingTheQuiz} ماه پیش`
+            } else if (currentDay > publishDay) {
+                const totalDaysAfterPublishingTheQuiz = currentDay - publishDay
+                return `${totalDaysAfterPublishingTheQuiz} روز پیش`
+            }
+        }
+        return publishFullDate
+    }
+
     return (
         props.quizzes.map((quiz) => {
             return (
@@ -28,7 +52,7 @@ const QuizContainer = (props) => {
                                 <img src={`${quiz.thumbnail}`} alt={`${quiz.subCategory}} | ${quiz.title}`} loading='lazy' />
                             </div>
                             {/* <div className="quizContainer__views">{viewsFormat(quiz.views)}</div> */}
-                            <div className="quizContainer__date">{replaceFunction(quiz.publish.slice(0, 10), '-', '/')}</div>
+                            <div className="quizContainer__date">{datePublishHandler(quiz.publish)}</div>
                             <span className="quizContainer__title quizContainer__title__noViews flex">
                                 { quiz.title }
                             </span>
