7fdd6afc3b89d368daf7385996a057c399e776b5
Mostafa-Code19 <tabrizian.codes@gmail.com>
2022-07-09 19:30:27 +0430

    fix sort not load when scroll infinite fix watchlist button in sort in mobile overflow hidden

--- frontend/src/pages/sort.js
+++ frontend/src/pages/sort.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import axiosInstance from '../components/axiosApi';
 import { Helmet } from "react-helmet";
 import InfiniteScroll from 'react-infinite-scroll-component';
-import { useLocation } from "react-router-dom";
 
 import LoadingScreen from '../components/loadingScreen'
 import QuizContainer from '../components/quizContainer'
@@ -25,8 +24,6 @@ const Sort = () => {
     const [offset, setOffset] = useState(0);
     const [useless, whenChangeThisIDKWhyTheSortAffect] = useState()
 
-    const location = useLocation();
-
     useEffect(() => {
         setOffset(0)
         setCountResult(100)
@@ -34,7 +31,7 @@ const Sort = () => {
         setLoadState(true)
         
         document.querySelector('body').style = 'background: #060101'
-        document.querySelector('#land').classList.add('overflow-y-auto')  // make content load on scroll
+        document.querySelector('#scrollme').classList.add('overflow-y-auto')  // make content load on scroll
     }, [])
 
     useEffect(() => {
@@ -59,23 +56,23 @@ const Sort = () => {
     }
 
     const fetchContent = async () => {
-        if (loading) {
-            return;
-        }
+        // if (loading) {
+        //     return;
+        // }
 
         setLoading(true)
 
         const quiz = await axiosInstance.get(`/api/quiz/?limit=${countResult}&offset=${offset}&public=true`)
         const pointy = await axiosInstance.get(`/api/test/?limit=${countResult}&offset=${offset}&public=true`)
         let content_new = quiz.data.results.concat(pointy.data.results)
-
+        
         setCountNewFetched(content_new.length)
 
         const sortCategory = takeParameterFromUrl('c')
         if (sortCategory) {
             content_new = content_new.filter(quiz => quiz.categoryKey.id == sortCategory)
         }
-
+        
         setContent([...content, ...content_new]);
         setOffset(offset + countResult)
         setLoading(false);
@@ -109,35 +106,40 @@ const Sort = () => {
                 {skeletonQuiz(contentLoaded)}
             </div>
 
-            <InfiniteScroll
-                dataLength={sortedContent.length}
-                next={fetchContent}
-                hasMore={countNewFetched >= 100 || countNewFetched != 0}
-                loader={
-                    <div className={`
-                        flex justify-center w-full
-                    `}>
-                        <div>
-                            <svg class="animate-spin h-10 w-10 m-10 text-red-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                            </svg>
+            <div id='scrollme' className='max-h-screen'>
+
+                <InfiniteScroll
+                    dataLength={sortedContent.length}
+                    next={fetchContent}
+                    hasMore={countNewFetched >= 100}  //  || countNewFetched != 0
+                    useWindow={true}
+                    loader={
+                        <div className={`
+                            flex justify-center w-full
+                        `}>
+                            <div>
+                                <button onClick={fetchContent}>Load more</button>
+                                <svg class="animate-spin h-10 w-10 m-10 text-red-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                                    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 0100 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                </svg>
+                            </div>
                         </div>
-                    </div>
-                }
-                endMessage={
-                    <div className='flex justify-center w-full mb-10'>
-                        <h2>
-                            این داستان ادامه دارد . . .
-                        </h2>
-                    </div>
-                }
-                scrollableTarget="land"
-            >
-                <ul className="mx-auto flex flex-wrap align-baseline w-[90vw] md:w-4/5 quizContainer flex-ai-fe justify-right pt-3">
-                    <QuizContainer quizzes={sortedContent} bgStyle='trans' />
-                </ul>
-            </InfiniteScroll>
+                    }
+                    endMessage={
+                        <div className='flex justify-center w-full mb-10'>
+                            <h2>
+                                این داستان ادامه دارد . . .
+                            </h2>
+                        </div>
+                    }
+                    scrollableTarget="scrollme"
+                >
+                    <ul className="mx-auto flex flex-wrap align-baseline w-[90vw] md:w-4/5 quizContainer flex-ai-fe justify-right pt-3">
+                        <QuizContainer quizzes={sortedContent} bgStyle='trans' />
+                    </ul>
+                </InfiniteScroll>
+            </div>
 
             {/* Adverts */}
 
