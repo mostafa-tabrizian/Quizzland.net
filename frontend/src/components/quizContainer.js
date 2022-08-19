d88253048c8e6781bd2f2d1a8393c8f0d8abac71
Mostafa-Code19 <tabrizian.codes@gmail.com>
2022-08-19 14:41:25 +0430

    return: hide onMouseLeave the quizContainer playlist button for some reason it was gone

--- frontend/src/components/quizContainer.js
+++ frontend/src/components/quizContainer.js
@@ -11,6 +11,7 @@ import userStore from '../store/userStore';
 
 const QuizContainer = (props) => {
     const [theme, setTheme] = useState('dark')
+    const [watchListButtonUnClickable, setWatchListButtonUnClickable] = useState(true)
     const [showPlaylistButton, setShowPlaylistButton] = useState(null)
 
     const [userProfile, userActions] = userStore()
@@ -25,6 +26,7 @@ const QuizContainer = (props) => {
             async (userDetail, updatedUserWatchList) => {
                 await axiosInstance.patch(`/api/userView/${userDetail.id}/`, { watch_list: updatedUserWatchList})
                     .then(res => {
+                        setWatchListButtonUnClickable(true)
                         message.error('با موفقیت از پلی لیست حذف گردید.')
                     })
                     .catch(err => {
@@ -98,6 +100,7 @@ const QuizContainer = (props) => {
             userActions.updatePlaylist(updatedPlaylist)
             await axiosInstance.patch(`/api/userView/${userDetail.id}/`, { watch_list: updatedPlaylist })
                 .then(res => {
+                    setWatchListButtonUnClickable(true)
                     message.success('با موفقیت به پلی لیست اضافه گردید.')
                 })
                 .catch(err => {
@@ -107,6 +110,8 @@ const QuizContainer = (props) => {
 	);
 
     const checkWatchList = async (quizId, quizCheckIfQuiz) => {
+        setWatchListButtonUnClickable(false)
+        
         let quizType
         if (quizCheckIfQuiz) {
             quizType = 'q'
@@ -123,8 +128,8 @@ const QuizContainer = (props) => {
     return (
         props.quizzes.map((quiz) => {
             return (
-                <li key={quiz.id} className='relative flex-auto mb-5 md:mr-4 md:mb-4'>
-                    <button onClick={() => addToWatchListClicked(quiz.id, quiz.GIF20)} className={`absolute top-[-0.5rem] right-[-.5rem] z-[1]`}>
+                <li key={quiz.id} className='relative flex-auto mb-5 md:mr-4 md:mb-4' onMouseEnter={() => setShowPlaylistButton(quiz.id)} onMouseLeave={() => setShowPlaylistButton(null)}>
+                    <button onClick={() => addToWatchListClicked(quiz.id, quiz.GIF20)} className={`${watchListButtonUnClickable?'':'pointer-events-none'} ${(showPlaylistButton == quiz.id) ? 'visible opacity-100 translate-y-0' : 'md:invisible md:opacity-0 md:translate-y-2'} duration-300 ease-in-out transition-all  absolute top-[-0.5rem] right-[-.5rem] z-[1]`}>
                         <svg class="h-7 w-7 text-[#ac272e]"  fill="#1e0809" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"/>
                         </svg>
